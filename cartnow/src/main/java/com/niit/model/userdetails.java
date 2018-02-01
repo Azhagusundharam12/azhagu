8d4b21ef15f4b830a652b8ecb9b7bbd0d12080bd
Azhagusundharam12 <31813216+Azhagusundharam12@users.noreply.github.com>
2018-02-01 20:52:57 +0530

    Add files via upload
    project

---
+++ cartnow/src/main/java/com/niit/model/userdetails.java
@@ -0,0 +1,41 @@
+package com.niit.model;
+
+public class userdetails {
+String firstname;
+String lastname;
+String username;
+String password;
+String emailid;
+public String getFirstname() {
+	return firstname;
+}
+public void setFirstname(String firstname) {
+	this.firstname = firstname;
+}
+public String getLastname() {
+	return lastname;
+}
+public void setLastname(String lastname) {
+	this.lastname = lastname;
+}
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getEmailid() {
+	return emailid;
+}
+public void setEmailid(String emailid) {
+	this.emailid = emailid;
+}
+
+
+}
